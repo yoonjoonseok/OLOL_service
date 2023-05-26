8dc76550a00ce057bc4e9c7a6e379a443ffd7484
Hjun1004 <hjun1004@naver.com>
2023-05-26 10:47:09 +0900

    first commit

---
+++ src/test/java/com/ll/olol/OlolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.olol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
