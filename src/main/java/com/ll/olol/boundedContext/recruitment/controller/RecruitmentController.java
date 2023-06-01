d53a4785655a3ba812baaa2391646f7e837128f7
Hjun1004 <hjun1004@naver.com>
2023-06-01 15:29:09 +0900

    refactor: 컨트롤러의 CreateForm을 따로 생성

--- src/main/java/com/ll/olol/boundedContext/recruitment/controller/RecruitmentController.java
+++ src/main/java/com/ll/olol/boundedContext/recruitment/controller/RecruitmentController.java
@@ -1,13 +1,11 @@
 package com.ll.olol.boundedContext.recruitment.controller;
 
+import com.ll.olol.boundedContext.recruitment.CreateForm;
 import com.ll.olol.boundedContext.recruitment.entity.RecruitmentArticle;
 import com.ll.olol.boundedContext.recruitment.entity.RecruitmentArticleForm;
 import com.ll.olol.boundedContext.recruitment.repository.RecruitmentRepository;
 import com.ll.olol.boundedContext.recruitment.service.RecruitmentService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,46 +25,6 @@ public class RecruitmentController {
 
     private final RecruitmentService recruitmentService;
 
-    @AllArgsConstructor
-    @Getter
-    public static class CreateForm {
-        //        @NotBlank
-//        @Size(min = 3, max = 65)
-        @NotBlank(message = "제목은 필수항목입니다.") // 아래의 변수는 비어있으면 안된다.
-        @Size(max = 65, message = "제목을 200자 이하로 설정해주세요.") // 최대 200까지 가능하다.
-        private String title;
-
-        @NotBlank(message = "내용은 필수항목입니다.")
-        @Size(max = 20000, message = "제목을 20000자 이하로 설정해주세요.")
-        private String content;
-
-
-        @NotNull(message = "타입은 필수항목입니다.")
-        @Min(value = 1, message = "올바른 타입 값을 선택해주세요.")
-        @Max(value = 2, message = "올바른 타입 값을 선택해주세요.")
-        private Integer typeValue;
-
-        @NotNull(message = "타입은 필수항목입니다.")
-        @Min(value = 1, message = "올바른 타입 값을 선택해주세요.")
-        @Max(value = 2, message = "올바른 타입 값을 선택해주세요.")
-        private Integer dayNight;
-
-        private String mountainName;
-
-        @NotNull(message = "모집인원은 필수항목입니다.")
-        @Min(value = 0, message = "올바른 인원수를 입력해주세요.")
-        private Long recruitsNumber;
-
-//        private LocalDateTime startTime;
-
-//        private LocalDateTime courseTime;
-
-        private Long ageRange;
-
-        @NotBlank(message = "내용은 필수항목입니다.")
-        private String connectType;
-
-    }
 
     @GetMapping("/create")
     // @Valid를 붙여야 QuestionForm.java내의 NotBlank나 Size가 동작한다.
