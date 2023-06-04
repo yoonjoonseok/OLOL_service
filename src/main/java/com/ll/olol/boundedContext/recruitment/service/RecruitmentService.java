09781309bca8291e5eb86f6be474b3d0d97298f5
Hjun1004 <hjun1004@naver.com>
2023-06-05 01:56:23 +0900

    feat: 저장되는 주소에서 동이 붙은 부분만 저장

--- src/main/java/com/ll/olol/boundedContext/recruitment/service/RecruitmentService.java
+++ src/main/java/com/ll/olol/boundedContext/recruitment/service/RecruitmentService.java
@@ -1,5 +1,6 @@
 package com.ll.olol.boundedContext.recruitment.service;
 
+import com.ll.olol.base.rsData.RsData;
 import com.ll.olol.boundedContext.recruitment.entity.RecruitmentArticle;
 import com.ll.olol.boundedContext.recruitment.entity.RecruitmentArticleForm;
 import com.ll.olol.boundedContext.recruitment.repository.RecruitmentFormRepository;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -37,17 +39,35 @@ public class RecruitmentService {
     }
 
     public void createArticleForm(RecruitmentArticle recruitmentArticle, Integer dayNight, Long recruitsNumber, String mountainName, Long ageRange, String connectType, LocalDateTime startTime, LocalDateTime courseTime) {
+        // 동만 붙은 부분만 가져옴
+        RsData<String> checkMt = mtAddressChecked(mountainName);
+        String realMountainAddress = checkMt.getData();
+
         RecruitmentArticleForm recruitmentArticleForm = new RecruitmentArticleForm();
 
         recruitmentArticleForm.setRecruitmentArticle(recruitmentArticle);
         recruitmentArticleForm.setDayNight(dayNight);
         recruitmentArticleForm.setRecruitsNumbers(recruitsNumber);
-        recruitmentArticleForm.setMountainName(mountainName);
+        recruitmentArticleForm.setMountainName(realMountainAddress);
         recruitmentArticleForm.setAgeRange(ageRange);
         recruitmentArticleForm.setConnectType(connectType);
         recruitmentArticleForm.setStartTime(startTime);
         recruitmentArticleForm.setCourseTime(courseTime);
 
         recruitmentFormRepository.save(recruitmentArticleForm);
+    }
+
+
+    private RsData<String> mtAddressChecked(String mountainName) {
+        String[] mtPullAddress = mountainName.split(" ");
+
+        String realMtAddress = Arrays.stream(mtPullAddress)
+                .filter(e -> e.endsWith("동"))
+                .findAny()
+                .orElse(null);
+
+        if (realMtAddress != null) return RsData.of("S-1", "동을 저장했습니다.", realMtAddress);
+
+        return RsData.of("F-1", "동을 저장 못함");
     }
 }
