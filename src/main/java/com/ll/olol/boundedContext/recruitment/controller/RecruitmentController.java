79cd12b26b1e20fc81f703442d9725d2d2d1033e
Hjun1004 <114876266+Hjun1004@users.noreply.github.com>
2023-06-12 01:22:44 +0900

    Feature/54 (#55)
    * fix: createDate와 modifyDate 자동으로 갱신되도록 수정
    
    * fix: merge후 버그 해결
    
    * feat: 신고 기능 추가
    
    * feat: admin 로그인 기능 추가
    
    * feat: 신고에 사유 지정 가능
    
    * feat: 관리자로 로그인 후 신고된 목록에서 신고된 게시글 들 볼수있도록 구현

--- src/main/java/com/ll/olol/boundedContext/recruitment/controller/RecruitmentController.java
+++ src/main/java/com/ll/olol/boundedContext/recruitment/controller/RecruitmentController.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -42,11 +43,13 @@ public class RecruitmentController {
     private final RecruitmentPeopleService recruitmentPeopleService;
     private int limitPeople = 0;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     // @Valid를 붙여야 QuestionForm.java내의 NotBlank나 Size가 동작한다.
     public String questionCreate2(CreateForm createForm) {
         return "recruitmentArticle/createRecruitment_form";
     }
+
 
     @PostMapping("/create")
     // @Valid QuestionForm questionForm
@@ -253,15 +256,6 @@ public class RecruitmentController {
         return "redirect:/recruitment/" + articleId;
     }
 
-
-//    @GetMapping("/list")
-//    public String list(Model model, @RequestParam(defaultValue = "0L") Long ageRange, @RequestParam(defaultValue = "0") int dayNight, @RequestParam(defaultValue = "0") int typeValue, @RequestParam(defaultValue = "1") int sortCode, @RequestParam(defaultValue = "0") int page, String kw) { // int page 가 곧 name = page와 같다.
-//        Page<RecruitmentArticle> paging = recruitmentService.getlist(page, kw);
-//        model.addAttribute("paging", paging);
-//        //model.addAttribute("kw",kw);
-//        return "usr/recruitment/allList";
-//    }
-
     @GetMapping("/list")
     public String list(Model model,
                        @RequestParam(defaultValue = "0") Long ageRange,
@@ -282,32 +276,6 @@ public class RecruitmentController {
         model.addAttribute("paging", paging);
         return "usr/recruitment/allList";
     }
-//    @PreAuthorize("isAuthenticated()")
-//    @GetMapping("/toList")
-//    public String showToList(Model model, @RequestParam(defaultValue = "") String gender, @RequestParam(defaultValue = "0") int attractiveTypeCode, @RequestParam(defaultValue = "1") int sortCode) {
-//        if (gender.trim().equals("")) gender = null;
-//
-//        InstaMember instaMember = rq.getMember().getInstaMember();
-//
-//        // 인스타인증을 했는지 체크
-//        if (instaMember != null) {
-//            Stream<LikeablePerson> likeablePeopleStream = instaMember.getToLikeablePeople().stream();
-//
-//            if (gender != null) {
-//                likeablePeopleStream = likeablePersonService.filterByGender(likeablePeopleStream, gender).getData();
-//            }
-//
-//            if (attractiveTypeCode != 0) {
-//                likeablePeopleStream = likeablePersonService.filterByAttractiveTypeCode(likeablePeopleStream, attractiveTypeCode).getData();
-//            }
-//
-//            likeablePeopleStream = likeablePersonService.sortCodeSroted(likeablePeopleStream, sortCode).getData();
-//
-//            List<LikeablePerson> likeablePeople = likeablePeopleStream.collect(Collectors.toList());
-//
-//            model.addAttribute("likeablePeople", likeablePeople);
-//        }
-//
-//        return "usr/likeablePerson/toList";
-//    }
+
+
 }
---
+++ src/main/java/com/ll/olol/boundedContext/report/controller/ReportController.java
@@ -0,0 +1,92 @@
+package com.ll.olol.boundedContext.report.controller;
+
+import com.ll.olol.base.rq.Rq;
+import com.ll.olol.base.rsData.RsData;
+import com.ll.olol.boundedContext.member.entity.Member;
+import com.ll.olol.boundedContext.recruitment.entity.RecruitmentArticle;
+import com.ll.olol.boundedContext.recruitment.service.RecruitmentService;
+import com.ll.olol.boundedContext.report.entity.ArticleReport;
+import com.ll.olol.boundedContext.report.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/recruitment")
+@RequiredArgsConstructor
+public class ReportController {
+    private final Rq rq;
+
+    private final ReportService reportService;
+
+    private final RecruitmentService recruitmentService;
+
+//    @GetMapping("/report")
+//    public String bookmark(Model model) {
+//        Member actor = rq.getMember();
+//        List<ArticleReport> reportRecruitmentArticles = reportService.findByFromMember(actor);
+//
+//        model.addAttribute("likeableRecruitmentArticles", likeableRecruitmentArticles);
+//        return "usr/member/bookmark";
+//    }
+
+    @PostMapping("/report/{id}")
+    public String add(@PathVariable Long id, @RequestParam int reason) {
+        Optional<RecruitmentArticle> or = recruitmentService.findById(id);
+
+        RecruitmentArticle recruitmentArticle = null;
+        if (or.isPresent()) {
+            recruitmentArticle = or.get();
+        }
+        Member actor = rq.getMember();
+
+        RsData canAddRsData = reportService.canReport(recruitmentArticle, actor);
+
+        if (canAddRsData.isFail())
+            return rq.historyBack(canAddRsData);
+
+
+        reportService.report(recruitmentArticle, actor, reason);
+
+        return "redirect:/recruitment/" + id;
+        //return "redirect:/recruit/bookmark";
+    }
+
+//    @PreAuthorize("hasAuthority('admin')")
+//    @GetMapping("/report/list")
+//    public String allReport(Model model) {
+//        List<ArticleReport> articleReportList = reportService.findAll();
+//        model.addAttribute(articleReportList);
+//
+//        return "adm/reportRecruitment/reportArticlelist";
+//    }
+
+
+    @PreAuthorize("hasAuthority('admin')")
+    @GetMapping("/report/list")
+    public String list(Model model,
+                       @RequestParam(defaultValue = "0") int reason,
+                       @RequestParam(defaultValue = "0") int page,
+                       String kw) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+
+
+        Pageable pageable = PageRequest.of(page, 20, Sort.by(sorts));
+        Page<ArticleReport> paging = reportService.getListByConditions(reason, kw, pageable);
+
+        model.addAttribute("paging", paging);
+        return "adm/reportRecruitment/reportArticlelist";
+    }
+
+}
