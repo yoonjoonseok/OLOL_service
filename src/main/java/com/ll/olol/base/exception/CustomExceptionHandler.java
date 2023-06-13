671665ca1a8e0965bebc76174ef3df92e7ced653
yoonjoonseok <yjsygm@naver.com>
2023-06-13 15:58:12 +0900

    Fix: 로그인되었으나 회원가입 되지 않은 경우 접근 차단

--- src/main/java/com/ll/olol/base/exception/CustomExceptionHandler.java
+++ src/main/java/com/ll/olol/base/exception/CustomExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 @RequiredArgsConstructor
 public class CustomExceptionHandler {
@@ -15,5 +17,10 @@ public class CustomExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     public String handleAccessDeniedException(AccessDeniedException ex, HttpServletRequest request) {
         return rq.historyBack("로그인을 선행해주세요");
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
+        return rq.historyBack("존재하지 않는 유저입니다");
     }
 }
