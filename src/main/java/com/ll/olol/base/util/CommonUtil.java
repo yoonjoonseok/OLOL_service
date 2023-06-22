afc8a8ee62f645c3f2041a90a15402f2a1ea8b99
JShistory <behappy0916@naver.com>
2023-06-22 15:50:45 +0900

    style: 주석 추가

--- src/main/java/com/ll/olol/base/util/CommonUtil.java
+++ src/main/java/com/ll/olol/base/util/CommonUtil.java
@@ -5,6 +5,9 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.stereotype.Component;
 
+/*
+마크 다운 문법을 적용시켜주는 util
+ */
 @Component
 public class CommonUtil {
     public String markdown(String markdown) {
