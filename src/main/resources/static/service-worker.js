e846a45aa903d5471f94f0ec57b5a3f310769e90
yoonjoonseok <yjsygm@naver.com>
2023-06-27 13:28:32 +0900

    Fix: 로그인/로그아웃 때만 서비스워커 등록/삭제, 히카리 커넥션 풀 사이즈 증가

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -17,20 +17,11 @@ eventSource.onmessage = function (event) {
 eventSource.onerror = function (event) {
     // 에러 처리 로직
     console.log("실패");
-
 }
 
-function getNotificationPermission() {
-    // 브라우저 지원 여부 체크
-    if (!("Notification" in window)) {
-        alert("데스크톱 알림을 지원하지 않는 브라우저입니다.");
+self.addEventListener('close', () => {
+    if (eventSource) {
+        eventSource.close();
     }
-    // 데스크탑 알림 권한 요청
-    Notification.requestPermission(function (result) {
-        // 권한 거절
-        if (result == 'denied') {
-            alert('알림을 차단하셨습니다.\n브라우저의 사이트 설정에서 변경하실 수 있습니다.');
-            return false;
-        }
-    });
-}
+    console.log("이벤트소스 클로즈");
+});
