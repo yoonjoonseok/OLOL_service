bf00c4969f421c23a5e41f0b47c6537b0d12e152
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-07-10 13:19:17 +0900

    refactor: 안정성을 위한 코드 수정

--- src/main/java/com/ll/olol/boundedContext/chat/controller/MessageController.java
+++ src/main/java/com/ll/olol/boundedContext/chat/controller/MessageController.java
@@ -21,21 +21,19 @@ public class MessageController {
 
     @MessageMapping("/chat/enter")
     public void enter(ChatMessageDTO message) {
-
         String roomId = message.getRoomId();
 
-        List<ChatMessage> messages = chatService.findByRoomId(roomId);
+        List<ChatMessage> messages = chatService.findMessagesByRoomId(roomId);
 
-        if (messages.size() == 0) {
-            message.setMessage(message.getWriter() + "님이 물건 구매를 원합니다.");
-            chatService.createChatMessage(message);
-            template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        if (messages.isEmpty()) {
+            ChatMessageDTO enterMessage = ChatMessageDTO.create(roomId, message.getWriter(), message.getWriter() + "님이 입장하였습니다.");
+            chatService.createChatMessage(enterMessage);
+            template.convertAndSend("/sub/chat/room/" + roomId, enterMessage);
         }
     }
 
     @MessageMapping("/chat/message")
     public void message(ChatMessageDTO message) {
-
         chatService.createChatMessage(message);
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
